47201ed5f2789b634bbf9352bdd0faa8459fe62b
NadeemPatel2019 <nadeempatel2019@u.northwestern.edu>
2025-06-08 03:31:25 -0500

    new portfolio sections

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/nadeempatel-portfolio/',
+  base: mode === 'production' ? '/nadeempatel-portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
